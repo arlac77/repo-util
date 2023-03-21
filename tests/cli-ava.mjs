b50c9a01f8daf20fb9444b6f2740f823d387b164
Markus Felten <Markus.Felten@gmx.de>
2023-03-21 09:11:46 +0100

    test: reformat

--- tests/cli-ava.mjs
+++ tests/cli-ava.mjs
@@ -1,14 +1,11 @@
 import test from "ava";
-import { execa } from "execa";
+import { execa } from "execa";
 
 test("cli list provider", async t => {
-  const p = await execa(
-    "node",
-    [
-      new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
-      "provider",
-    ]
-  );
+  const p = await execa("node", [
+    new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
+    "provider"
+  ]);
 
   const m = p.stdout.match(/github:/);
   t.truthy(m);
@@ -17,31 +14,25 @@ test("cli list provider", async t => {
 });
 
 test.serial("cli list branches", async t => {
-  const p = await execa(
-    "node",
-    [
-      new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
-      "branch",
-      "*repository-*"
-    ]
-  );
+  const p = await execa("node", [
+    new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
+    "branch",
+    "*repository-*"
+  ]);
 
   const m = p.stdout.match(/repository/);
- // t.truthy(m);
+  // t.truthy(m);
 
   t.is(p.exitCode, 0);
 });
 
 test.serial("cli list repositories as json", async t => {
-  const p = await execa(
-    "node",
-    [
-      new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
-      "repository",
-      "--json",
-      "*/*repository*"
-    ]
-  );
+  const p = await execa("node", [
+    new URL("../src/repo-util-cli.mjs", import.meta.url).pathname,
+    "repository",
+    "--json",
+    "*/*repository*"
+  ]);
 
   t.is(p.exitCode, 0);
 
